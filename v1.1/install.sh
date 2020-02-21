a33a704902cd06d6f4fea54c40d4ef695eaa29e0
Charles Bao <charles@mathcrowd.cn>
2020-02-21 19:24:03 +0800

    (2020.02.20) v1.1 example added, README updated.

--- v1.1/install.sh
+++ v1.1/install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 cd $(dirname $0)
-mkdir -p ~/.local/share/fonts/
-ln -vnfs $(pwd)/fonts ~/.local/share/fonts/myfonts
+mkdir -p /usr/local/share/fonts
+ln -vnfs $(pwd)/fonts /usr/local/share/fonts/myfonts
 mkdir -p ~/texmf/tex/latex
-ln -vnfs $(pwd)/BHCexam.cls ~/texmf/tex/latex/BHCexam.cls
+ln -vnfs $(pwd)/BHCexam.cls /usr/local/texlive/texmf-local/tex/latex/BHCexam.cls
 mktexlsr
 updmap-sys
 sudo apt install -y fontconfig
